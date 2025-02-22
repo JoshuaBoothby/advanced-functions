3d6c936e9141a604684ae307766814d375ea39e7
Joshua Boothby <Mona.Lisa@example.com>
2025-02-21 21:10:29 -0600

    finished constructor

--- constructorFunctions.js
+++ constructorFunctions.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function Person(name, age, isEdgerunner) {
+  this.name = name;
+  this.age = age;
+  this.isEdgerunner = isEdgerunner;
+}
+
+const personCon = new Person("Frank", 35, true);
+console.log(personCon);
