c7fa401f33b00dfcb5416a98335fc2d86498b2bf
Joshua Boothby <Mona.Lisa@example.com>
2025-02-21 21:10:39 -0600

    finished prototype

--- prototypes.js
+++ prototypes.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function Animal(name, species, cyberEnhanced) {
+  this.name = name;
+  this.species = species;
+  this.cyberEnhanced = cyberEnhanced;
+}
+Animal.prototype.build = function () {
+  console.log(
+    this.name +
+      " is a " +
+      this.species +
+      " and is cyber enhanced " +
+      this.cyberEnhanced
+  );
+};
+const normalDog = new Animal("Truman", "dog", false);
+const cyberCat = new Animal("Whiskers", "cat", true);
+normalDog.build();
+cyberCat.build();
