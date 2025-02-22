fd5509721ea2df1feebadc5da3e3803ea7bbc4b7
Joshua Boothby <Mona.Lisa@example.com>
2025-02-21 21:10:13 -0600

    finished factory function

--- factoryFunctions.js
+++ factoryFunctions.js
@@ -1,1 +1,15 @@
 "use strict";
+
+function createPerson(name, age, isEdgerunner) {
+  const person = {
+    name: name,
+    age: age,
+    isEdgerunner: isEdgerunner,
+  };
+  return person;
+}
+
+const person1 = createPerson("Chuck", 38, true);
+const person2 = createPerson("Brian", 85, false);
+console.log(person1);
+console.log(person2);
